5b202ed4fc362618450b21c7592d028c65b1c097
sergio <sergio_dr@hotmail.com.ar>
2020-04-29 20:11:59 -0300

    'tercero'

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ app.use('/api/tareas', require('./routes/tareas'));
 
 //arrancar la app 
 app.listen(port, '0.0.0.0' , () => {
-    console.log(`El servidor esta funcionando en el puerto ${PORT}`)
+    console.log(`El servidor esta funcionando en el puerto ${port}`)
 });
 
 //definir la pagina principal 
